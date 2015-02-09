f43d86d0cdcd158befcdb54f4158c6889447fe62
Erik Bülow <erik.bulow@rcvast.se>
2015-02-09 14:43:53 +0100

    better warning message when x transformed for decodeing and spaces trimmed if any.

--- R/internal_format_as_key.R
+++ R/internal_format_as_key.R
@@ -3,6 +3,7 @@
 # Try to transform x in order to fit the key from keyvalue
 format_as_key <- function(x, keyvalue){
     
+    ## Get the keyvalue object if specified by character name
     if (is.character(keyvalue)){
         keyvalue <- get(keyvalue)
     }
@@ -10,35 +11,48 @@ format_as_key <- function(x, keyvalue){
     x_name <- deparse(substitute(x))
     keyvalue_name <- deparse(substitute(keyvalue))
     
+    # initiate warning message
+    msg <- ""
+    
+    has_blank    <- function(y) any(grepl("[[:blank:]]", y))
     has_punct    <- function(y) any(grepl("[[:punct:]]", y))
     has_capitals <- function(y) any(grepl("[[:upper:]]", y))
     has_anylow   <- function(y) any(grepl("[[:lower:]]", y))
     
+    # Trim spaces 
+    if (has_blank(x)){
+        x <- stringr::str_trim(x)
+        msg <- "Spaces are removed from beginning and end."
+    }
+    
     # If x has punctuations but the key has not, punctuations are removed from x
     if (has_punct(x) & !has_punct(keyvalue$key)){
         x <- stringr::str_replace_all(x, "[[:punct:]]", "")
-        warning(x_name, " contained punctuation. Punctuations are removed and capital letters",
-                " are changed to lower case before decoding!")
+        msg <- paste(msg, "Punctuations are removed")
     }
     
     # If key has no capitals, all letters in x are made lowercase
     if (has_capitals(x) & !has_capitals(keyvalue$key)){
         x <- tolower(x)
-        warning(x_name, " contains upper case letters but the key does not. Capitals are changed to lower case!")
+        msg <- paste(msg, "Capitals are changed to lower case.")
         
         
-        # If key has no capitals, all letters in x are made lowercase
+    # If key has no lowercase, all letters in x are made upper case
     } else if (has_anylow(x) & !has_anylow(keyvalue$key)){
         x <- toupper(x)
-        warning(x_name, " contains lower case letters but the key does not. Lower case are changed to upper case!")
+        msg <- paste(msg, "Lower case are changed to upper case.")
     }
     
     # If the key is shorter than x, x is substringed
     key_length <- max(stringr::str_length(keyvalue$key), na.rm = TRUE)
     if (max(stringr::str_length(x), na.rm = TRUE) > key_length){
         x <- substr(x, 1, key_length)
-        warning("Entries in ", x_name, " have too many characters to be matched to ",
-                x_name, " and is threfore substringed to match its length!")
+        msg <- paste(msg, "Only the first", key_length, "characters are used")
+    }
+    
+    ## Print warning if any changes made
+    if (msg != ""){
+        warning(x_name, "has been transformed to match", keyvalue_name, ":", msg)
     }
     
     x
