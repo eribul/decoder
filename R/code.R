bf214854ad50e4a53173a523abd76a963d52e4bd
Erik Bülow <erik.bulow@rcvast.se>
2015-05-04 16:05:02 +0200

    m

--- R/code.R
+++ R/code.R
@@ -3,17 +3,17 @@
 #' @export
 code <- function(y, keyvalue, verbose = TRUE){
     
-    if (is.character(keyvalue)){
+    if (is.character(keyvalue)) {
         keyvalue <- get(keyvalue)
     }
     
     ## If there is a no 1:1 relation between key and value,
     ## the process continues just if the particular translation can be done safely!
-    if (!is.keyvalue11(keyvalue)){
+    if (!is.keyvalue11(keyvalue)) {
         dangerous_values <- summary(keyvalue)$nonunique$value
         not11 <- y %in% dangerous_values
-        if (any(not11)){
-            if (verbose){ 
+        if (any(not11)) {
+            if (verbose) { 
                 print(summary(keyvalue)$nonunique[dangerous_values %in% y, ])
                 stop("Values above have a non 1:1 relation to their key. \nA safe coding can not be made for this variable!")
             } else{
