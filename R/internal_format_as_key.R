c6939829cee374e82f6174454a4f8580ce945afd
Erik Bülow <erik.bulow@rcvast.se>
2015-05-05 11:21:41 +0200

    Bug fix. Fix #41.

--- R/internal_format_as_key.R
+++ R/internal_format_as_key.R
@@ -4,7 +4,7 @@
 format_as_key <- function(x, keyvalue, name_x = "x", name_keyvalue = "keyvalue"){
     
     ## Get the keyvalue object if specified by character name
-    if (is.character(keyvalue)){
+    if (is.character(keyvalue)) {
         keyvalue <- get(keyvalue)
     }
     
@@ -18,29 +18,17 @@ format_as_key <- function(x, keyvalue, name_x = "x", name_keyvalue = "keyvalue")
     start_0      <- function(y) any(substr(as.character(y), 1, 1) == 0, na.rm = TRUE) 
     
     # Trim spaces 
-    if (has_blank(x)){
+    if (has_blank(x)) {
         x <- stringr::str_trim(x)
         msg <- "Spaces are removed from beginning and end."
     }
     
-    ## If key is numeric & if removing leading 0:s from key is safe:
-    ## leading 0:s are removed from both x and key
-    if (all(rccmisc::is_numeric(keyvalue$key), na.rm = TRUE) && 
-        !any(duplicated(rccmisc::as_numeric(keyvalue$key)), na.rm = TRUE)){ 
-      
-        if (!all(x == suppressWarnings(rccmisc::as_numeric(x)), na.rm = TRUE)){
-            msg <- paste(msg, "Coerced to numeric.")        
-        }
-        x <- suppressWarnings(rccmisc::as_numeric(x))
-        keyvalue$key <- rccmisc::as_numeric(keyvalue$key)
-    }
-    
     # If x starts with zero but the key do not, zeroes are removed
     # This applies even if x is still character (and is therefore not an ifelse statement)
-    if (start_0(x) & !start_0(keyvalue$key)){
+    if (start_0(x) & !start_0(keyvalue$key)) {
         msg <- paste(msg, "Leading 0:s are ignored.")
     }
-    while (start_0(x) & !start_0(keyvalue$key)){
+    while (start_0(x) & !start_0(keyvalue$key)) {
         x <- ifelse(substr(x, 1, 1) == "0", # If leading 0 ...
                     substring(x, 2),        # ... remove the 0 ...
                     x)                      # ... otherwise leave unchanged
@@ -52,39 +40,39 @@ format_as_key <- function(x, keyvalue, name_x = "x", name_keyvalue = "keyvalue")
     if (!start_0(x) && 
         start_0(keyvalue$key) && 
         do.call(all.equal, as.list(na.omit(stringr::str_length(keyvalue$key)))) && 
-        key_length > min(stringr::str_length(x), na.rm = TRUE)){
+        key_length > min(stringr::str_length(x), na.rm = TRUE)) {
             x <- stringr::str_pad(x, key_length, pad = "0")
     msg <- paste(msg, "Leading 0:s are introduced for short characters.")
     }
     
     
     # If x has punctuations but the key has not, punctuations are removed from x
-    if (has_punct(x) & !has_punct(keyvalue$key)){
+    if (has_punct(x) & !has_punct(keyvalue$key)) {
         x <- stringr::str_replace_all(x, "[[:punct:]]", "")
         msg <- paste(msg, "Punctuations are removed")
     }
     
     # If key has no capitals, all letters in x are made lowercase
-    if (has_capitals(x) && has_anylow(keyvalue$key)  && !has_capitals(keyvalue$key)){
+    if (has_capitals(x) && has_anylow(keyvalue$key)  && !has_capitals(keyvalue$key)) {
         x <- tolower(x)
         msg <- paste(msg, "Capitals are changed to lower case.")
         
         
     # If key has no lowercase, all letters in x are made upper case
-    } else if (has_anylow(x) && has_capitals(keyvalue$key) && !has_anylow(keyvalue$key)){
+    } else if (has_anylow(x) && has_capitals(keyvalue$key) && !has_anylow(keyvalue$key)) {
         x <- toupper(x)
         msg <- paste(msg, "Lower case are changed to upper case.")
     }
     
     # If the key is shorter than x, x is substringed
     key_length <- max(stringr::str_length(keyvalue$key), na.rm = TRUE)
-    if (max(stringr::str_length(x), na.rm = TRUE) > key_length){
+    if (max(stringr::str_length(x), na.rm = TRUE) > key_length) {
         x <- substr(x, 1, key_length)
         msg <- paste(msg, "Only the first", key_length, "characters are used.")
     }
     
     ## Print warning if any changes made
-    if (msg != ""){
+    if (msg != "") {
         warning(name_x, " has been transformed to match the code for ", name_keyvalue, 
                 ": ", msg, call. = FALSE
         )
--- tests/testthat/test-decode.R
+++ tests/testthat/test-decode.R
@@ -17,7 +17,7 @@ suppressWarnings({
         expect_that(length(decode(snomed$key, "snomed")), is_equivalent_to(length(snomed$key)))
         expect_that(decode("      0000001", "kon"), gives_warning())
         expect_that(decode("      0000001", "kon"), is_equivalent_to("Man"))
-        expect_that(decode(c("0002425", 2425, "df"), "kommun"), is_equivalent_to(c("Dorotea", "Dorotea", NA)))
+        expect_that(decode(c(0002425, 2425, "df"), "kommun"), is_equivalent_to(c("Dorotea", "Dorotea", NA)))
     })
     
     
